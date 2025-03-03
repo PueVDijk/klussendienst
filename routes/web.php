bbd82edf9b4f87bdaf020732168266f57227a43a
Ahmed <ahmedmohalghurbani@gmail.com>
2025-03-03 15:34:46 +0100

    Providers Routes aangemaakt

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AssignmentController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProviderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -27,6 +28,18 @@ Route::name("assigments.")->group(function () {
         Route::get('/{assigment}/edit', [AssignmentController::class, 'edit'])->name('edit');
         Route::put('/{assigment}', [AssignmentController::class, 'update'])->name('update');
         Route::delete('/{assigment}', [AssignmentController::class, 'destroy'])->name('destroy');
+    });
+});
+
+Route::name("providers.")->group(function () {
+    Route::prefix("providers")->group(function () {
+        Route::get('/', [ProviderController::class, 'index'])->name('index');
+        Route::get('/create', [ProviderController::class, 'create'])->name('create');
+        Route::post('/', [ProviderController::class, 'store'])->name('store');
+        Route::get('/{provider}', [ProviderController::class, 'show'])->name('show');
+        Route::get('/{provider}/edit', [ProviderController::class, 'edit'])->name('edit');
+        Route::put('/{provider}', [ProviderController::class, 'update'])->name('update');
+        Route::delete('/{provider}', [ProviderController::class, 'destroy'])->name('destroy');
     });
 });
 
