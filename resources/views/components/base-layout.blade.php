4b9faff340f1aa1a408b0734792bd109b3945cbd
Pieter van Dijk <p.u.e.van.dijk@outlook.com>
2025-03-17 15:04:48 +0100

    als gast zie je register, als je al bent ingelogd niet

--- resources/views/components/base-layout.blade.php
+++ resources/views/components/base-layout.blade.php
@@ -83,7 +83,9 @@
 
                     <nav class="flex items-center space-x-1 ml-6">
                         <a href="/home" class="nav-link px-4 py-2 text-white font-medium rounded-lg">Home</a>
-                        <a href="/register" class="nav-link px-4 py-2 text-white font-medium rounded-lg">Register</a>
+                        @guest
+                            <a href="/register" class="nav-link px-4 py-2 text-white font-medium rounded-lg">Register</a>
+                        @endguest
                         <a href="/assignments" class="nav-link px-4 py-2 text-white font-medium rounded-lg">Assignments</a>
                         <a href="/users" class="nav-link px-4 py-2 text-white font-medium rounded-lg">Users</a>
                     </nav>
