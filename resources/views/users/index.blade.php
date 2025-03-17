5772d7bd8ae56943446e8c369fb30e1ecdc108e1
Pieter van Dijk <p.u.e.van.dijk@outlook.com>
2025-03-17 14:00:16 +0100

    alle users worden getoond

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -13,33 +13,16 @@
                     </tr>
                 </thead>
                 <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
-                    @foreach ($handymen as $handyman)
+                    @foreach ($users as $user)
                         <tr class="hover:bg-gray-50 dark:hover:bg-gray-700">
-                            <td class="px-6 py-4">{{ $handyman->id }}</td>
-                            <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">{{ $handyman->firstname }}</td>
-                            <td class="px-6 py-4">{{ $handyman->lastname }}</td>
-                            <td class="px-6 py-4">{{ $handyman->role }}</td>
+                            <td class="px-6 py-4">{{ $user->id }}</td>
+                            <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">{{ $user->firstname }}</td>
+                            <td class="px-6 py-4">{{ $user->lastname }}</td>
+                            <td class="px-6 py-4">{{ $user->role }}</td>
                             <td class="px-6 py-4 text-center">
-                                <a href="/users/{{ $handyman->id }}" class="px-3 py-1 text-sm font-medium text-blue-600 bg-blue-100 rounded-full dark:bg-blue-600 dark:text-white hover:bg-blue-200 dark:hover:bg-blue-500">View</a>
-                                <a href="/users/{{ $handyman->id }}/edit" class="px-3 py-1 text-sm font-medium text-green-600 bg-green-100 rounded-full dark:bg-green-600 dark:text-white hover:bg-green-200 dark:hover:bg-green-500 ml-2">Edit</a>
-                                <form action="/users/{{ $handyman->id }}" method="POST" style="display:inline;">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="px-3 py-1 text-sm font-medium text-red-600 bg-red-100 rounded-full dark:bg-red-600 dark:text-white hover:bg-red-200 dark:hover:bg-red-500 ml-2">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                    @foreach ($providers as $provider)
-                        <tr class="hover:bg-gray-50 dark:hover:bg-gray-700">
-                            <td class="px-6 py-4">{{ $provider->id }}</td>
-                            <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">{{ $provider->firstname }}</td>
-                            <td class="px-6 py-4">{{ $provider->lastname }}</td>
-                            <td class="px-6 py-4">{{ $provider->role }}</td>
-                            <td class="px-6 py-4 text-center">
-                                <a href="/users/{{ $provider->id }}" class="px-3 py-1 text-sm font-medium text-blue-600 bg-blue-100 rounded-full dark:bg-blue-600 dark:text-white hover:bg-blue-200 dark:hover:bg-blue-500">View</a>
-                                <a href="/users/{{ $provider->id }}/edit" class="px-3 py-1 text-sm font-medium text-green-600 bg-green-100 rounded-full dark:bg-green-600 dark:text-white hover:bg-green-200 dark:hover:bg-green-500 ml-2">Edit</a>
-                                <form action="/users/{{ $provider->id }}" method="POST" style="display:inline;">
+                                <a href="/users/{{ $user->id }}" class="px-3 py-1 text-sm font-medium text-blue-600 bg-blue-100 rounded-full dark:bg-blue-600 dark:text-white hover:bg-blue-200 dark:hover:bg-blue-500">View</a>
+                                <a href="/users/{{ $user->id }}/edit" class="px-3 py-1 text-sm font-medium text-green-600 bg-green-100 rounded-full dark:bg-green-600 dark:text-white hover:bg-green-200 dark:hover:bg-green-500 ml-2">Edit</a>
+                                <form action="/users/{{ $user->id }}" method="POST" style="display:inline;">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="px-3 py-1 text-sm font-medium text-red-600 bg-red-100 rounded-full dark:bg-red-600 dark:text-white hover:bg-red-200 dark:hover:bg-red-500 ml-2">Delete</button>
