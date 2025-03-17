db5e495708b909ec651a774c095d1989d22a92dc
Pieter van Dijk <p.u.e.van.dijk@outlook.com>
2025-03-17 14:23:12 +0100

    rol begint met hoofdletter

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -18,7 +18,7 @@
                             <td class="px-6 py-4">{{ $user->id }}</td>
                             <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">{{ $user->firstname }}</td>
                             <td class="px-6 py-4">{{ $user->lastname }}</td>
-                            <td class="px-6 py-4">{{ $user->role }}</td>
+                            <td class="px-6 py-4">{{ ucfirst($user->role) }}</td>
                             <td class="px-6 py-4 text-center">
                                 <a href="/users/{{ $user->id }}" class="px-3 py-1 text-sm font-medium text-blue-600 bg-blue-100 rounded-full dark:bg-blue-600 dark:text-white hover:bg-blue-200 dark:hover:bg-blue-500">View</a>
                                 <a href="/users/{{ $user->id }}/edit" class="px-3 py-1 text-sm font-medium text-green-600 bg-green-100 rounded-full dark:bg-green-600 dark:text-white hover:bg-green-200 dark:hover:bg-green-500 ml-2">Edit</a>
